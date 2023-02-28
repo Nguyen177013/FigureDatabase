30a5f2b24a5c8ffa7e5e81d434c2c3f3a4643162
Nguyen177013 <phannguyendoremon@gmail.com>
2023-02-28 15:41:54 +0700

    adding npm and install Express

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log("Ngày mai học Nodejs");
+const express = require('express');
+const morgan = require("morgan");
+const port = 3000;
+const app = express();
+app.use(morgan("dev"));  
+app.listen(3000,()=>{
+    console.log("server listening on port",port);
+})
+app.get('/',(req,res)=>{
+    res.send("xin chào nodejs");
+})
