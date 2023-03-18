3e01d831578ced12767a76adaa4c6d03e25a13f3
ShiroYukiGY <101681198+ShiroYukiGY@users.noreply.github.com>
2023-03-18 12:58:18 +0700

    chinh sua controller fig

--- controller/figure.js
+++ controller/figure.js
@@ -1,27 +1,95 @@
 const Figure = require('../models/Figure');
-const Artist = require('../models/artists');
-const Categories = require('../models/Categories');
-const Characters = require('../models/Characters');
-const Companies = require('../models/Companys');
-const Materials = require('../models/materials');
-const Origins = require('../models/Origins')
-const clound = require('../cloudinary');
-const path = require('path');
-const storage = multer.diskStorage({
-    destination:(req,file,cb)=>{
-        cb(null,path.join(__dirname, '..','public',"img"))
-    },
-        filename:(req,file,cb)=>{
-            cb(null,Date.now()+path.extname(file.originalname));
-    },
-});
+// const postFavorate = require('../models/postFavorate');
+// const Artist = require('../models/artists');
+// const Categories = require('../models/Categories');
+// const Characters = require('../models/Characters');
+// const Companies = require('../models/Companys');
+// const Materials = require('../models/materials');
+// const Origins = require('../models/origin');
+const Views = require('../models/userView');
+const Favorate = require('./favorate');
+const Comment = require('../controllers/comment');
+const Post = require('../models/userpost');
+const thisDate = new Date();
+class FigureController{
+    async index (req,res){
+        try{
+            let thisMonth = thisDate.getMonth()+1;
+            let thisYear = thisDate.getFullYear();
+            const latest = Figure.find().populate('category').populate('artists').populate('character').sort({_id: -1}).limit(6);
+            let [late,topView,month] = await Promise.all([latest,Views.sortView(),Figure.getByMonth(thisMonth,thisYear,0,6)]);
+            res.render('Home/index',{figures:late,months:month,views:topView});
+            }
+            catch(ex){
+                console.log(ex.message);
+            }
+        }
+        async latest_fig(req,res){
+        const figPerPage = 15;
+        const length = await Figure.find().count();
+        const page = req.params.p-1;
+        const latest = await Figure.find().populate('category').populate('artists').populate('character')
+        .sort({_id: -1}).skip(figPerPage * page).limit(figPerPage);
+        res.render('Figure/latest',{length,page,latest:latest});
+    }
+    async figThismonth(req,res){
+        let thisMonth = thisDate.getMonth()+1;
+        let thisYear = thisDate.getFullYear(); 
+        const page = req.params.p-1;
+        const limit = 15;
+        let length = await Figure.getByMonth(thisMonth,thisYear,0,0);
+        let data = await Figure.getByMonth(thisMonth,thisYear,page,limit);
+        res.render('Figure/months',{length:length.length,page,latest:data});
+    }
+    async topview(req,res){
+        const page = req.params.p-1;
+        const limit = 15;
+        const data = await Views.sortView(page,limit);
+        const length = await Views.sortView(0,0);
+        console.log(length);
+        res.render('Figure/fires',{length:length.length,page,latest:data});
+    }
+    async figure_detail(req,res){
+        const fig_id = req.params.id;
+        let userId = res?.locals?.user?.id;
+        const figure = Figure.findById(fig_id).populate('category')
+        .populate('artists').populate('character')
+        .populate('origin').populate('company').populate('materials');
+        const views =  Views.find({figure:fig_id}).count();
+        const total = Favorate.totalFavorate(fig_id);
+        let comments = Comment.getAllComment(fig_id);
+        let check = Favorate.checkUser(userId,fig_id);
+        let involve = Post.find({character:fig_id});
+        const [a,b,c,d,e,f] = await Promise.all([figure,views,total,comments,check,involve]);
+        let displayPost =[];
+        f.map(ele=>{
+            return displayPost.push(ele.images[0].url);
+        })
+        res.render('Home/detail',{title:"detail",figure:a,views:b,favorate:c,comments:d,check:e,displayPost});
+    }
+    async itemFigure(req,res){
+        const figure = await Figure.find();
+        res.render('Figure/items',{figures: figure,status:'fire'});
+    }
+    async findFigure(req,res){
+        let searchName = req.query.name;
+        const figure = await Figure.find({name:{'$regex':searchName,$options: 'is'}});
+        res.render('Figure/find',{name:searchName,figures:figure});
+    }
+    async findNav(req,res){
+        try{
+            let searchName = req.body.name;
+            if(searchName){
+                const figure = await Figure.find({name:{'$regex':searchName,$options: 'is'}}).populate('origin');
+            res.json({figures:figure});
+        }
+        else
+        res.json({figures:''});
+    }
+    catch(e){
+        console.log(e.message);
+    }
+    }
+}
 
-
-const upload = multer({
-    storage:storage,
-    limits:{
-        fileSize: 1024 * 1024 * 25
-    },
-}).array('img',20);
-
-class f
+module.exports = new FigureController;
