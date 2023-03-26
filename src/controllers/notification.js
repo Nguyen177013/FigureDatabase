b24de991617dca5a242ff0bdea24fade12863028
Xuan Phuong <xuanphuong26102001@gmail.com>
2023-03-26 10:27:27 +0700

    update controller

---
+++ src/controllers/notification.js
@@ -0,0 +1,14 @@
+const notificate = require('../models/notification');
+
+class notificationController {
+    async removeNotify(req, res) {
+        try {
+            let { id } = req.body;
+            let result = await notificate.remove({ user: id });
+            res.json(result)
+        } catch (ex) {
+            console.log(ex.message);
+        }
+    }
+}
+module.exports = new notificationController;
